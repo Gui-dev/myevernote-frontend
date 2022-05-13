18169ff3fc0797aeb9a8411ccc0feb480b089fd4
Gui-dev <adrianon10@gmail.com>
2022-05-13 03:47:24 -0300

    feat: add Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react'
+import { FaBars, FaTimes } from 'react-icons/fa'
 
-import styles from './style.module.scss';
+import logoImg from './../../assets/images/logo-white.png'
+import styles from './style.module.scss'
 
 export const Header = () => {
+  const [isOpenMenu, setIsOpenMenu] = useState(false)
+
+  const handleToggleMenu = () => {
+    setIsOpenMenu(prev => !prev)
+  }
+
   return (
-    <h1>Header</h1>
+    <section className={styles.container}>
+      <header className={styles.header}>
+        <img src={logoImg} alt="" />
+
+        <div className={styles.buttonMenu} onClick={handleToggleMenu}>
+          { isOpenMenu
+            ? <FaTimes size={32} color="#FFF"/>
+            : <FaBars size={32} color="#FFF"/>
+          }
+        </div>
+
+        <ul className={`${styles.navbar} ${isOpenMenu ? styles.active : ''} `}>
+          <li>
+            <a href="#">Item 1</a>
+          </li>
+          <li>
+            <a href="#">Item 1</a>
+          </li>
+          <li>
+            <a href="#">Item 1</a>
+          </li>
+        </ul>
+      </header>
+
+    </section>
   )
 }
