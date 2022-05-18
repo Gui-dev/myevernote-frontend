3bd7a726d67a50249ca4268f4535f9894a3356a5
Gui-dev <adrianon10@gmail.com>
2022-05-18 03:45:40 -0300

    feat: add helper function handleSubmit

--- src/components/RegisterForm/index.tsx
+++ src/components/RegisterForm/index.tsx
@@ -1,22 +1,55 @@
 import React, { FormEvent, useState } from 'react'
 import { FaLock, FaMailBulk, FaUserAlt } from 'react-icons/fa'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { AxiosError } from 'axios'
 
 import styles from './style.module.scss'
+import { api } from '../../services/api'
 
 export const RegisterForm = () => {
+  const navigate = useNavigate()
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [error, setError] = useState('')
+  const [loading, setLoading] = useState(false)
 
-  const handleSubmit = (event: FormEvent) => {
+  const handleSubmit = async (event: FormEvent) => {
     event.preventDefault()
-    console.log(name, email, password)
+
+    try {
+      setLoading(true)
+      if (name === '' || email === '' || password === '') {
+        setError('todos os campos devem ser preenchidos')
+        return
+      }
+
+      await api.post('/users/register', {
+        name,
+        email,
+        password
+      })
+
+      navigate('/login')
+    } catch (error) {
+      const err = error as AxiosError
+
+      if (err.response?.status === 400) {
+        setError('Este e-mail já foi cadastrado, tente outro e-mail')
+      }
+    } finally {
+      setLoading(false)
+    }
   }
 
   return (
     <form className={styles.container} onSubmit={ handleSubmit }>
+      { error && (
+        <div className={styles.messageError}>
+          { error }
+        </div>
+      ) }
+
       <div className={styles.inputGroup}>
         <FaUserAlt size={18} color="#FFF"/>
         <input
@@ -51,7 +84,11 @@ export const RegisterForm = () => {
       </div>
 
       <div className={styles.buttonGroup}>
-        <button>Cadastrar</button>
+        <button>
+          {
+            loading ? 'Carregando...' : 'Cadastrar'
+          }
+        </button>
         <Link to="/login">
           Já tenho uma conta, fazer login
         </Link>
