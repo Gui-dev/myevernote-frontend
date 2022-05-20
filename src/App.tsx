4536b447de90ba998f6edadccb77aef90e1d0a5c
Gui-dev <adrianon10@gmail.com>
2022-05-20 03:45:15 -0300

    feat: add context AuthProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 
+import { AuthProvider } from './contexts/auth'
 import { Header } from './components/Header'
 import { RoutesNavigation } from './routes'
 
 function App () {
-  const user = true
   return (
-    <BrowserRouter>
-      {user && (
+    <AuthProvider>
+      <BrowserRouter>
         <Header />
-      )}
-      <RoutesNavigation />
-    </BrowserRouter>
+        <RoutesNavigation />
+      </BrowserRouter>
+    </AuthProvider>
   )
 }
 
