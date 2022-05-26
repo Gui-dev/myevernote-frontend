eca5e891e740ec0bdfd017da30c13fdd26d87227
Gui-dev <adrianon10@gmail.com>
2022-05-26 04:08:01 -0300

    feat: add page Notes

---
+++ src/pages/Notes/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { NotesComponent } from '../../components/Notes'
+import styles from './style.module.scss'
+
+export const Notes = () => {
+  return (
+    <NotesComponent />
+  )
+}
