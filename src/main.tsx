9bd1fb4a3dc785fe77d1f7d90cb90ac186303aa0
Gui-dev <adrianon10@gmail.com>
2022-05-31 04:19:11 -0300

    feat: add config for react-modal

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Modal from 'react-modal'
+
 import App from './App'
 
 import './main.scss'
+
+Modal.setAppElement('#root')
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
