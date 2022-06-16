96335eb1fcac16af27c8c46b28cbb16d99d24441
Gui-dev <adrianon10@gmail.com>
2022-06-16 04:00:02 -0300

    feat: add helper function handleUpdatePersonalInformation

--- src/pages/User/index.tsx
+++ src/pages/User/index.tsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { FormEvent, useState } from 'react'
 import { AiOutlineArrowLeft } from 'react-icons/ai'
 import { FaLock, FaMailBulk, FaUserAlt } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
+import { useAuth } from '../../hooks/useAuth'
 
 import styles from './style.module.scss'
 
 export const User = () => {
+  const { user, updatePersonalInformation, updatePassword } = useAuth()
+  const [name, setName] = useState(user?.name || '')
+  const [email, setEmail] = useState(user?.email || '')
+  const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
+
+  const handleUpdatePersonalInformation = async (event: FormEvent) => {
+    event.preventDefault()
+    await updatePersonalInformation(name, email)
+  }
+
+  const handleUpdatePassword = async (event: FormEvent) => {
+    event.preventDefault()
+    await updatePassword(password, confirmPassword)
+  }
+
   return (
     <section className={styles.container}>
       <Link to="/notes">
@@ -13,15 +30,15 @@ export const User = () => {
       </Link>
       <div className={styles.personalInformation}>
         <h1>Informações pessoais</h1>
-        <form>
+        <form onSubmit={handleUpdatePersonalInformation}>
           <div className={styles.inputGroup}>
             <FaUserAlt size={18} color="#FFF"/>
             <input
               type="text"
               name="name"
               placeholder="Nome"
-              // value={name}
-              // onChange={(event) => setName(event.target.value)}
+              value={name}
+              onChange={(event) => setName(event.target.value)}
             />
           </div>
           <div className={styles.inputGroup}>
@@ -30,26 +47,31 @@ export const User = () => {
               type="email"
               name="email"
               placeholder="E-mail"
-              // value={email}
-              // onChange={(event) => setEmail(event.target.value)}
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
             />
           </div>
 
-          <button className={styles.buttonSubmit}>Atualizar</button>
+          <button
+            type="submit"
+            className={styles.buttonSubmit}
+          >
+            Atualizar Perfil
+          </button>
         </form>
       </div>
 
       <div className={styles.fieldPassword}>
         <h1>Alterar a Senha</h1>
-        <form>
+        <form onSubmit={handleUpdatePassword}>
           <div className={styles.inputGroup}>
             <FaLock size={18} color="#FFF"/>
             <input
               type="password"
               name="password"
               placeholder="Senha"
-              // value={password}
-              // onChange={(event) => setPassword(event.target.value)}
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
             />
           </div>
 
@@ -59,11 +81,16 @@ export const User = () => {
               type="password"
               name="confirPassword"
               placeholder="Confirmar Senha"
-              // value={password}
-              // onChange={(event) => setPassword(event.target.value)}
+              value={confirmPassword}
+              onChange={(event) => setConfirmPassword(event.target.value)}
             />
           </div>
-          <button className={styles.buttonSubmit}>Atualizar</button>
+          <button
+            type="submit"
+            className={styles.buttonSubmit}
+          >
+            Atualizar Senha
+          </button>
         </form>
       </div>
     </section>
